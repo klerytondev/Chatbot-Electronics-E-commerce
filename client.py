8fcd09cd7a5c8a9347d06821da0bc768d8206572
Kleryton Souza <kleryton.dev@gmail.com>
2024-11-04 20:41:01 -0300

    Code refactored

--- client.py
+++
@@ -1,5 +0,0 @@
-from langserve import RemoteRunnable
-
-remote_chain = RemoteRunnable("http://localhost:8000/tradutor")
-response = remote_chain.invoke({"idioma": "ingles", "texto": "eu te amo"})
-print(response)
--- model_selection.py
+++ model_selection.py
@@ -10,26 +10,14 @@ model, *_ = initial_parameters()
 model_name = model.model_name 
 codificador = tiktoken.encoding_for_model(model_name)
 
-# def model(prompt_system_model, prompt_usuario):
-#     token_list = codificador.encode(prompt_system_model + prompt_usuario)
-#     number_tokens = len(token_list)
-#     print(f"Número de tokens na entrada: {number_tokens}")
-#     expected_output_size = 2048
-#     selected_model = ChatOpenAI(model="gpt-4o-mini")
-#     # if number_tokens >= 4096 - expected_output_size:
-#     #     selected_model = ChatOpenAI(model="gpt-4o")
-#     print(f"Modelo selecionado: {selected_model.model_name}")
-
-#     return selected_model
-
 def model_str(prompt_system_model, prompt_usuario):
     token_list = codificador.encode(prompt_system_model + prompt_usuario)
     number_tokens = len(token_list)
     print(f"Número de tokens na entrada: {number_tokens}")
     expected_output_size = 2048
     selected_model = ChatOpenAI(model="gpt-4o-mini")
-    # if number_tokens >= 4096 - expected_output_size:
-    #     selected_model = ChatOpenAI(model="gpt-4o")
+    if number_tokens >= 4096 - expected_output_size:
+        selected_model = ChatOpenAI(model="gpt-4o")
     print(f"Modelo selecionado: {selected_model.model_name}")
 
     return selected_model.model_name
